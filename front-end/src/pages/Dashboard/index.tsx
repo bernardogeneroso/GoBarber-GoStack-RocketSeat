4baae8ef3abc30cf2fc4481a4aaa3d67c5df02c2
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2020-10-03 17:10:21 +0100

    Update design Schedule

--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { FiPower } from "react-icons/fi";
+import React, { useEffect, useState } from "react";
+import { FiPower, FiClock } from "react-icons/fi";
 
 import { useAuth } from "../../hooks/Auth";
 import {
@@ -9,11 +9,16 @@ import {
   Profile,
   Content,
   Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from "./styles";
 import logoImg from "../../assets/start/logo.svg";
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   useEffect(() => {
@@ -41,7 +46,116 @@ const Dashboard: React.FC = () => {
       </Header>
 
       <Content>
-        <Schedule></Schedule>
+        <Schedule>
+          <h1>Scheduled times</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 3</span>
+            <span>Sexta-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Attendance below</strong>
+
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                alt="Bernardo Generoso"
+              />
+
+              <strong>Bernardo Generoso</strong>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                11:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                  alt="Bernardo Generoso"
+                />
+                <strong>Bernardo Generoso</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                12:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                  alt="Bernardo Generoso"
+                />
+                <strong>Bernardo Generoso</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                15:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                  alt="Bernardo Generoso"
+                />
+                <strong>Bernardo Generoso</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                16:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                  alt="Bernardo Generoso"
+                />
+                <strong>Bernardo Generoso</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                17:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/58465456?s=460&u=05a258c08282945ed4792189821c5b8ac17907de&v=4"
+                  alt="Bernardo Generoso"
+                />
+
+                <strong>Bernardo Generoso</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
         <Calendar />
       </Content>
     </Container>
