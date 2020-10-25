9f0277f614b80796bedaafff379ed7256fc02cdd
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2020-10-25 10:30:20 +0000

    Mobile add & forgot password

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,154 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useCallback, useRef} from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
+import * as Yup from 'yup';
+
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
+
+import {useAuth} from '../../hooks/Auth';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+interface SigInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const navigation = useNavigation();
+
+  const {signIn} = useAuth();
+
+  const handleSignIn = useCallback(
+    async (data: SigInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('The E-mail field is required')
+            .email('Write a valid email'),
+          password: Yup.string().required('The Password field is required'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const erros = getValidationErrors(err);
+
+          formRef.current?.setErrors(erros);
+
+          return;
+        }
+
+        console.log(err);
+
+        Alert.alert('Error in authentication', err.response.data.message);
+      }
+    },
+    [signIn],
+  );
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          contentContainerStyle={{flex: 1}}
+          keyboardShouldPersistTaps="handled">
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Log In</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignIn} style={{width: '100%'}}>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+              />
+              <Input
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Password"
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}>
+                Log In
+              </Button>
+            </Form>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('Aqui');
+              }}>
+              <ForgotPasswordText>I forgot my password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+
+import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
+
+import {useAuth} from '../hooks/Auth';
+
+const Routes: React.FC = () => {
+  const {user, loading} = useAuth();
+
+  if (loading) {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
+  return user ? <AppRoutes /> : <AuthRoutes />;
+};
+
+export default Routes;
